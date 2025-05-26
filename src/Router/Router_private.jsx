85d07ceb9d2b82132599dae3079a6e4e416ee66c
Eduardo Valero <evaleromolina@gmail.com>
2025-05-26 10:59:05 +0200

    Poke78

--- src/Router/Router_private.jsx
+++ src/Router/Router_private.jsx
@@ -1,13 +1,13 @@
-import { Navigate ,Outlet} from "react-router-dom";
-export function Rutas_privdas(){
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase"; 
 
-    let auth = false;
+export function Rutas_privdas() {
+  const [user, loading] = useAuthState(auth);
 
+  if (loading) return <p>Cargando...</p>; 
 
-    return(
-
-        auth ? <Outlet />:<Navigate to = "/" />
-
-    )
+  return user ? <Outlet /> : <Navigate to="/" />;
 }
-export default Rutas_privdas
+
+export default Rutas_privdas;
